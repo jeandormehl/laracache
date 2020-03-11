79c6c1aad884171739563cfc0868a8034084568d
Robson Tenório <robson.tenorio@tjdft.jus.br>
2020-03-11 11:38:53 -0300

    Add Laravel 7.0  support
    - Laravel 7.0
    - PHP 7.4
    - Phpunit 8.5
    - Adjust dropColumn test for compatibility

--- tests/CacheSchemaGrammarTest.php
+++ tests/CacheSchemaGrammarTest.php
@@ -192,7 +192,7 @@ class CacheSchemaGrammarTest extends TestCase
         $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
 
         $this->assertEquals(1, \count($statements));
-        $this->assertEquals('alter table users drop column foo', $statements[0]);
+        $this->assertStringContainsString('alter table users drop column foo', $statements[0]);
     }
 
     public function testDropPrimary()
