d1798d6f20b72a8fcf497a62d0c57a58d9a2ea82
Robson Tenório <rrtenorio@gmail.com>
2024-12-03 19:30:25 -0300

    WIP

--- src/Cache/Query/Grammars/Grammar.php
+++ src/Cache/Query/Grammars/Grammar.php
@@ -20,6 +20,8 @@ class Grammar extends SqlServerGrammar
 
         if (empty($components['orders'])) {
             $components['orders'] = 'order by 1';
+            $components['offset'] = null;
+            $components['limit'] = null;
         }
 
         $components['columns'] = $this->compileOver($this->columnize($query->columns));
@@ -32,6 +34,20 @@ class Grammar extends SqlServerGrammar
     {
         return "select top all {$orderings}";
     }
+
+    protected function compileRowConstraint($query)
+    {
+        $start = (int) $query->offset + 1;
+
+        if ($query->limit > 0) {
+            $finish = (int) $query->offset + (int) $query->limit;
+
+            return "between {$start} and {$finish}";
+        }
+
+        return ">= {$start}";
+    }
+
 
     protected function compileTableExpression($sql, $query)
     {
